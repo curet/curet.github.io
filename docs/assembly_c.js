ec2e1acee6e9862283e324f937583d0748cf437c
José C.S. Curet <jcscuret@gmail.com>
2020-08-10 15:22:13 -0400

    adding variables

--- docs/assembly_c.js
+++ docs/assembly_c.js
@@ -1,19 +1,30 @@
 
-function replace_test() {
-  var original = document.getElementById("demo").innerHTML;
-  var modified = document.getElementById("new_demo").innerHTML;
+let a = "demo"
+let b = "new_demo"
+
+function replace_test(a, b) {
+  
+  var original = document.getElementById(a).innerHTML;
+  var modified = document.getElementById(b).innerHTML;
 //   for(i=0; i< (original.length - 1); i++){
 //     var replaced = original.replace(original[i], modified[i]);
 //   }
    var replaced = original.replace(original, modified) 
   
-  document.getElementById("demo").innerHTML = replaced;
+  document.getElementById(a).innerHTML = replaced;
+
+  return replaced;
 }
 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 850 || document.documentElement.scrollTop > 850) {
-    replace_test()
+    replace_test(a, b)
   }
+
+  // if (document.body.scrollTop < 850 || document.documentElement.scrollTop < 850) {
+  //   replace_test(b, a)
+  // }
+
 }
