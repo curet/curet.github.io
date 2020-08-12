2d223afd74bd4c2a9f94ebd4d0dc62e333e447d2
José C.S. Curet <jcscuret@gmail.com>
2020-08-12 18:15:21 -0400

    final result ready
    now work as planned, from assembly to C, but later will review to clean the code and make better code decisions I have in mind and to test the page in different browsers and devices

--- docs/assembly_c.js
+++ docs/assembly_c.js
@@ -1,45 +1,11 @@
 
-function replace_text(x, y) {
-  
-  let original = document.getElementById(x).innerHTML;
-  let modified = document.getElementById(y).innerHTML;
-//   for(i=0; i< (original.length - 1); i++){
-//     var replaced = original.replace(original[i], modified[i]);
-//   }
-   let replaced = original.replace(original, modified) 
-  
-  document.getElementById(x).innerHTML = replaced;
-
-  // return replaced;
-}
-
-function replace_text_b(x, y) {
-  
-  let original = document.getElementById(x).innerHTML;
-  let modified = document.getElementById(y).innerHTML;
-//   for(i=0; i< (original.length - 1); i++){
-//     var replaced = original.replace(original[i], modified[i]);
-//   }
-   let replaced = original.replace(original, modified) 
-  
-  document.getElementById(x).innerHTML = replaced;
-
-  // return replaced;
-}
-
-// let assembly_to_c = replace_text('assembly_code', 'c_code');
-// c_to_assembly = replace_text('c_code', 'assembly_code');
-
-
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 920 || document.documentElement.scrollTop > 920) {
-    // assembly_to_c
-    let assembly_to_c = replace_text('assembly_code', 'c_code');
-    // assembly_to_c
+    document.getElementById('assembly_code').style.display = "none"
   }
   else{
-    // let c_to_assembly = replace_text('c_code', 'assembly_code');
+    document.getElementById('assembly_code').style.display = "contents"
   }
 }
